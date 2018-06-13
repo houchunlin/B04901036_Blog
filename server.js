3a3e79797a66df99cb29a7683d1da748b5e44ab1
Turtle25 <b04901036@ntu.edu.tw>
2018-06-13 16:38:18 +0800

    v0.1 missed file

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var Article = require('./models/Article.js');
+
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost/BlogB04901036', { promiseLibrary: require('bluebird') })
+  .then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'false'}));
+
+app.listen(3125);
+
+app.get('/', function(req, res, next) {
+  Article.find(function (err, data) {
+    if (err) return next(err);
+    res.json(data);
+  });
+});
+
+app.get('/:id', function(req, res, next) {
+  Article.findById(req.params.id, function (err, data) {
+    if (err) return next(err);
+    res.json(data);
+  });
+})
+
+app.post('/', function(req, res, next) {
+  Article.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+app.put('/:id', function(req, res, next) {
+  Article.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+app.delete('/:id', function(req, res, next) {
+  Article.findByIdAndRemove(req.params.id, req.body, function (err, del) {
+    if (err) return next(err);
+    res.json(del);
+  });
+});
